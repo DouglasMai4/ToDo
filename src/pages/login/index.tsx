177883aee574004abb8512d7c7afd7b61589957a
Douglas Maia <douglasmai4@protonmail.com>
2023-06-28 17:21:14 -0300

    feat: Login page finished

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -7,9 +7,34 @@ import styles from './login.module.scss';
 export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMsg, setErrorMsg] = useState({
+    username: '',
+    password: '',
+  });
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+    
+    if (!username) {
+      setErrorMsg({
+        username: 'Usuário é obrigatório!',
+        password: '',
+      });
+      return;
+    }
+
+    if (password.length < 6) {
+      setErrorMsg({
+        username: '',
+        password: 'A senha deve ter o tamanho de >= 6!',
+      });
+      return;
+    }
+
+    setErrorMsg({
+      username: '',
+      password: '',
+    });
   }
 
   return (
@@ -29,6 +54,9 @@ export default function Login() {
           setUsername(e.currentTarget.value);
         }}
       />
+      {errorMsg.username && (
+        <span className={ styles.errorMsg }>{errorMsg.username}</span>
+      )}
 
       <Input
         id="password"
@@ -41,6 +69,9 @@ export default function Login() {
           setPassword(e.currentTarget.value);
         }}
       />
+      {errorMsg.password && (
+        <span className={ styles.errorMsg }>{errorMsg.password}</span>
+      )}
 
       <button
         type="submit"
